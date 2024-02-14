170730ca84bc9f1ddce1bf15e8b8bf8a98bf0371
Phoenixhacker76 <78677838+Phoenixhacker76@users.noreply.github.com>
2024-02-14 21:05:14 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -42,7 +42,7 @@ class Config(object):
 	BOT_USERNAME = os.environ.get("BOT_USERNAME" , "Brookv2_probot")
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001838078113"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1425888381"))
-	DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://drakestreality:drakestreality@darkreality.xsnj5id.mongodb.net/?retryWrites=true&w=majority")
+	DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://allmoviesarehere:allmoviesarehere@cluster0.4pcf4u7.mongodb.net/?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002121447195")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1002109144230")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
